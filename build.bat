8db57a9e3c57c6cffb7439151bf20ad4e5079c43
小类 <joyanhui@qq.com>
2022-09-24 18:59:21 +0000

    完善自动生成bat
    Signed-off-by: 小类 <joyanhui@qq.com>

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-SET binDir="./bin/"
+SET binDir=.\bin\
 
 SET CGO_ENABLED=0
 :  ====== x86 64
@@ -6,33 +6,33 @@ echo "windows x86 64  webdav.exe"
 SET GOARCH=amd64
 SET GOOS=windows
 go build  main.go
-del %binDir%webdav.exe & ren main.exe %binDir%webdav.exe
+del %binDir%webdav.exe & move main.exe %binDir%webdav.exe
 
 echo "linux x86 64 webdav"
 SET GOARCH=amd64
 SET GOOS=linux
 go build  main.go
-del %binDir%webdav & ren main %binDir%webdav
+del %binDir%webdav & move main %binDir%webdav
 
 echo "macos x86 64 webdav"
 SET GOARCH=amd64
 SET GOOS=darwin
 go build  main.go
-del %binDir%webdav_darwin & ren main %binDir%webdav_darwin
+del %binDir%webdav_darwin & move main %binDir%webdav_darwin
 
 :  ====== x86 32
 echo "windows x86 32 webdav32.exe"
 SET GOARCH=386
 SET GOOS=windows
 go build  main.go
-del %binDir%webdav32.exe & ren main.exe %binDir%webdav32.exe
+del %binDir%webdav32.exe & move main.exe %binDir%webdav32.exe
 
 
 echo "linux x86 32 webdav32"
 SET GOARCH=386
 SET GOOS=linux
 go build  main.go
-del %binDir%webdav32 &ren  main  %binDir%webdav32
+del %binDir%webdav32 & move  main  %binDir%webdav32
 
 echo "macos 32 ... jump "
 :  ======
@@ -41,13 +41,13 @@ echo "windows arm  webdavArm.exe"
 SET GOARCH=arm
 SET GOOS=windows
 go build  main.go
-del %binDir%webdavArm.exe & ren  main.exe  %binDir%webdavArm.exe
+del %binDir%webdavArm.exe & move  main.exe  %binDir%webdavArm.exe
 
 echo "linux arm  webdavArm"
 SET GOARCH=arm
 SET GOOS=linux
 go build  main.go
-del %binDir%webdavArm & ren  main  %binDir%webdavArm
+del %binDir%webdavArm & move  main  %binDir%webdavArm
 
 
 echo "macos arm64  .. pls go to macos"
@@ -55,5 +55,6 @@ echo "macos arm64  .. pls go to macos"
 
 
 
-ECHO close
+
+
 PAUSE >nul
