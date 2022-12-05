40e191a9bd07c84253103bbfd7cd1af1a3af1ba4
joyanhui <joyanhui@qq.com>
2022-12-05 08:44:34 +0800

    编译后自动提交-

--- build.sh
+++ build.sh
@@ -18,12 +18,12 @@ rm -rf $binDir/webdavgo_arm_x64.exe
 mv main.exe $binDir/webdavgo_arm_x64.exe
 
 echo "linux x86 64  webdavgo_x64"
-CGO_ENABLED=0 GOOS=linux GOARCH=arm go build main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build main.go
 rm -rf $binDir/webdavgo_x64
 mv main $binDir/webdavgo_x64
 
 echo "linux x86 32  webdavgo_x86"
-CGO_ENABLED=0 GOOS=linux GOARCH=arm go build main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build main.go
 rm -rf $binDir/webdavgo_x86
 mv main $binDir/webdavgo_x86
 
@@ -32,5 +32,24 @@ CGO_ENABLED=0 GOOS=linux GOARCH=arm go build main.go
 rm -rf $binDir/webdavgo_arm_x64
 mv main $binDir/webdavgo_arm_x64
 
+
+
+echo "macos x86 64   webdavgo_darwin_x64"
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build main.go
+rm -rf $binDir/webdavgo_darwin_x64
+mv main $binDir/webdavgo_darwin_x64
+
+echo "macos arm 64  webdavgo_darwin_arm_x64"
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm go build main.go
+rm -rf $binDir/webdavgo_darwin_arm_x64
+mv main $binDir/webdavgo_darwin_arm_x64
+
 echo "clean ... "
 rm -rf __debug_bin*
+
+echo "git  ... "
+git add .
+
+git commit -m "编译后自动提交-$RANDOM"
+# git pull
+git push -u origin master 
